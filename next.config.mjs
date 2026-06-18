259993ab61a5583ea49c7cda49440b9ea40473c3
Chhaavii <chhaavii@example.com>
2026-06-18 18:29:27 -0400

    Rebuild and redeploy portfolio with static export configuration

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+export default nextConfig
